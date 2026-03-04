1bf702ce5bdeea0490ba626e5155c80837c2925e
christhesyrian <cbeshara17@gmail.com>
2026-03-04 14:05:50 -0800

    Updated CLI to include Features

--- src/cmapps_telemetry_anomaly_detection/cli.py
+++ src/cmapps_telemetry_anomaly_detection/cli.py
@@ -1,9 +1,13 @@
 import argparse
 from cmapps_telemetry_anomaly_detection.data_ingestion.kaggle_data_import import download_dataset
 from cmapps_telemetry_anomaly_detection.data_preprocessing.data_preprocess import run_preprocessing
+from cmapps_telemetry_anomaly_detection.feature_extraction.feature_extraction import run_feature_extraction
+
 
 def main():
+    # Create top-level parser
     p = argparse.ArgumentParser(prog="cmapps-tad")
+    # Add subcommands
     sub = p.add_subparsers(dest="cmd")
 
     # Create subcommands ONCE
@@ -14,9 +18,12 @@ def main():
         help="Re-download even if files exist (ignores .gitkeep)",
     )
 
+    # Add other subcommands
     sub.add_parser("train", help="Train anomaly model(s)")
     sub.add_parser("score", help="Score anomalies")
     sub.add_parser("preprocess", help="Preprocess raw CMAPSS data")
+    sub.add_parser("features", help="Extract features from processed data")
+
 
 
     args = p.parse_args()
@@ -31,5 +38,7 @@ def main():
         print("No command provided. Use --help for usage information.")
     elif args.cmd == "preprocess":
         run_preprocessing()
+    elif args.cmd == "features":
+        run_feature_extraction()
     else:
         p.print_help()
